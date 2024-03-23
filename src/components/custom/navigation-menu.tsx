5f0d49f2cec1db5e9083b999809d9c280632b22c
Thato Will Joseph <programergeek@gmail.com>
2024-03-23 17:53:04 +0200

    feat: mobile nav

--- src/components/custom/navigation-menu.tsx
+++ src/components/custom/navigation-menu.tsx
@@ -7,14 +7,22 @@ import {
   navigationMenuTriggerStyle,
 } from "../ui/navigation-menu";
 import { Button } from "../ui/button";
-import { Menu } from "lucide-react";
+import {
+  BookText,
+  CalendarDays,
+  Glasses,
+  Home,
+  Menu,
+  UserCircle,
+  X,
+} from "lucide-react";
 import { useState } from "react";
 
 const NavMenu: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <div className="fixed top-0 grid h-14 w-full grid-cols-3 bg-white">
-      <div id="logo" className="pl-20 pt-4">
+    <div className="fixed top-0 grid h-14 w-full grid-cols-2 bg-white lg:grid-cols-3">
+      <div id="logo" className="pl-5 pt-4 lg:pl-20">
         <p className="font-playfair">Motswana Mind</p>
       </div>
       <div id="menu" className="hidden w-full place-items-center lg:grid">
@@ -51,7 +59,7 @@ const NavMenu: React.FC = () => {
           </NavigationMenuList>
         </NavigationMenu>
       </div>
-      <div id="user" className="flex justify-end pr-20 pt-2">
+      <div id="user" className="flex justify-end pt-2 lg:pr-20">
         <div className="hidden gap-3 font-della lg:flex">
           <Button className="w-fit align-middle">Login</Button>
           <Button variant={"outline"}>Signup</Button>
@@ -59,26 +67,70 @@ const NavMenu: React.FC = () => {
         <Button
           size={"icon"}
           variant={"ghost"}
-          className="rounded-full bg-white focus:bg-white lg:hidden"
+          className="rounded-full bg-white pr-5 focus:bg-white lg:hidden"
+          onClick={() => setIsOpen(!isOpen)}
         >
           <Menu />
         </Button>
       </div>
-      <div className="absolute h-screen w-screen bg-accent">
-        <ul>
+      <div
+        className="absolute h-screen w-screen bg-accent px-5 pt-20 transition-all lg:hidden"
+        style={{
+          transform: isOpen ? "translateX(0)" : "translateX(100%)",
+        }}
+      >
+        <Button
+          size={"icon"}
+          variant={"ghost"}
+          className="absolute right-5 top-5 rounded-full bg-accent focus:bg-accent lg:hidden"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          <X color="#fff" />
+        </Button>
+        <ul className="text-white">
           <li>
-            <Link to="/">Home</Link>
+            <Link to="/">
+              <Button className="flex w-full justify-start gap-3 bg-accent text-base">
+                <Home className="h-5 w-5" />
+                Home
+              </Button>
+            </Link>
           </li>
           <li>
-            <Link to="/">Learn</Link>
+            <Link to="/">
+              <Button className="flex w-full justify-start gap-3 bg-accent text-base">
+                <BookText />
+                Learn
+              </Button>
+            </Link>
           </li>
           <li>
-            <Link to="/">Resources</Link>
+            <Link to="/">
+              <Button className="flex w-full justify-start gap-3 bg-accent text-base">
+                <Glasses />
+                Resources
+              </Button>
+            </Link>
           </li>
           <li>
-            <Link to="/">Events</Link>
+            <Link to="/">
+              <Button className="flex w-full justify-start gap-3 bg-accent text-base">
+                <CalendarDays />
+                Events
+              </Button>
+            </Link>
           </li>
         </ul>
+        <Link to="/">
+          <div className="absolute bottom-5 flex w-[90%] gap-5 rounded-lg border-2 border-white p-5 sm:w-[94%] md:w-[96%]">
+            <div>
+              <UserCircle color="#fff" />
+            </div>
+            <div className="grid h-full place-items-center font-semibold text-white">
+              Login / Sign up
+            </div>
+          </div>
+        </Link>
       </div>
     </div>
   );
