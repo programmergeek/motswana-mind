68e0323ba1cf9366c8ddf25131d5c2bf0f323864
Thato Will Joseph <programergeek@gmail.com>
2024-03-23 16:56:54 +0200

    style: make topics section responsive

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -38,77 +38,88 @@ function Index() {
             </div>
           </section>
         </div>
-        <section className="p-5 xl:px-64">
-          <div className="grid min-h-64 w-full grid-cols-1 gap-10 rounded-[50px] bg-gray-200 px-10 py-16 2xl:container md:px-36 lg:grid-cols-2 lg:px-32 lg:py-24">
-            <div>
-              <p className="text-xl">
-                <span className="font-bold">Chapter 1</span> Numbers &
-                Operations
-              </p>
-              <ul className="mt-5 flex flex-col gap-2 pl-5 font-medium">
-                <li>1.1 Whole Numbers</li>
-                <li>1.2 Directed Numbers</li>
-                <li>1.3 Number Operations</li>
-                <li>1.4 Fractions</li>
-                <li>1.5 Decimals</li>
-                <li>1.6 Percentages</li>
-                <li>1.7 Rates and Ratios</li>
-                <li>1.8 Money</li>
-              </ul>
+        <section className="py-10 xl:px-64">
+          <div className="grid min-h-64 w-full grid-cols-1 gap-10 rounded-[50px] bg-gray-200 px-10 py-16 2xl:container sm:grid-cols-2 md:px-36 lg:px-32 lg:py-24 2xl:grid-cols-3">
+            <div className="flex gap-3">
+              <img src="/icons/numbers.png" alt="Numbers" className="h-10" />
+              <div>
+                <p className="text-xl">
+                  <span className="font-bold">Chapter 1</span> Numbers &
+                  Operations
+                </p>
+                <ul className="mt-5 flex flex-col gap-2 pl-5 font-medium">
+                  <li>1.1 Whole Numbers</li>
+                  <li>1.2 Directed Numbers</li>
+                  <li>1.3 Number Operations</li>
+                  <li>1.4 Fractions</li>
+                  <li>1.5 Decimals</li>
+                  <li>1.6 Percentages</li>
+                  <li>1.7 Rates and Ratios</li>
+                  <li>1.8 Money</li>
+                </ul>
+              </div>
             </div>
-            <div className="">
-              <p className="text-xl">
-                <span className="font-bold">Chapter 2</span> Geometry
-              </p>
-              <ul className="mt-5 flex flex-col gap-2 pl-5 font-medium">
-                <li>
-                  2.1{" "}
-                  <div>
-                    <p className="text-xl">Quick Links</p>
-                    <div className="mt-5 flex flex-col gap-3">
-                      <p>Home</p>
-                      <p>Learn</p>
-                      <p>Resources</p>
-                      <p>Events</p>
-                    </div>
-                  </div>
-                  Angles
-                </li>
-                <li>2.2 Polygons</li>
-                <li>2.3 Transformations</li>
-                <li>2.4 Coordinate Geometry</li>
-              </ul>
+            <div className="flex gap-3">
+              <img src="/icons/geometry.png" alt="Geometry" className="h-10" />
+              <div className="">
+                <p className="text-xl">
+                  <span className="font-bold">Chapter 2</span> Geometry
+                </p>
+                <ul className="mt-5 flex flex-col gap-2 pl-5 font-medium">
+                  <li>2.1 Angles</li>
+                  <li>2.2 Polygons</li>
+                  <li>2.3 Transformations</li>
+                  <li>2.4 Coordinate Geometry</li>
+                </ul>
+              </div>
             </div>
-            <div>
-              <p className="text-xl">
-                <span className="font-bold">Chapter 3</span> Measures
-              </p>
-              <ul className="mt-5 flex flex-col gap-2 pl-5 font-medium">
-                <li>3.1 Length</li>
-                <li>3.2 Area</li>
-                <li>3.3 Mass</li>
-                <li>3.4 Volume</li>
-                <li>3.5 Time</li>
-              </ul>
+            <div className="flex gap-3">
+              <img
+                src="/icons/measuring-tape.png"
+                alt="measuring tape"
+                className="h-10"
+              />
+              <div>
+                <p className="text-xl">
+                  <span className="font-bold">Chapter 3</span> Measures
+                </p>
+                <ul className="mt-5 flex flex-col gap-2 pl-5 font-medium">
+                  <li>3.1 Length</li>
+                  <li>3.2 Area</li>
+                  <li>3.3 Mass</li>
+                  <li>3.4 Volume</li>
+                  <li>3.5 Time</li>
+                </ul>
+              </div>
             </div>
-            <div className="">
-              <p className="text-xl">
-                <span className="font-bold">Chapter 4</span> Statistics
-              </p>
-              <ul className="mt-5 flex flex-col gap-2 pl-5 font-medium">
-                <li>4.1 Graph</li>
-                <li>4.2 Measures of central tendency</li>
-                <li>4.3 Data collection and manipulation</li>
-                <li>4.4 Probability</li>
-              </ul>
+            <div className="flex gap-3">
+              <img
+                src="/icons/analytics.png"
+                alt="Analytics"
+                className="h-10"
+              />
+              <div>
+                <p className="text-xl">
+                  <span className="font-bold">Chapter 4</span> Statistics
+                </p>
+                <ul className="mt-5 flex flex-col gap-2 pl-5 font-medium">
+                  <li>4.1 Graph</li>
+                  <li>4.2 Measures of central tendency</li>
+                  <li>4.3 Data collection and manipulation</li>
+                  <li>4.4 Probability</li>
+                </ul>
+              </div>
             </div>
-            <div>
-              <p className="text-xl">
-                <span className="font-bold">Chapter 5</span> Algebra
-              </p>
-              <ul className="mt-5 flex flex-col gap-2 pl-5 font-medium">
-                <li>5.1 Algebra</li>
-              </ul>
+            <div className="flex gap-3">
+              <img src="/icons/graph.png" alt="Graph" className="h-10" />
+              <div>
+                <p className="text-xl">
+                  <span className="font-bold">Chapter 5</span> Algebra
+                </p>
+                <ul className="mt-5 flex flex-col gap-2 pl-5 font-medium">
+                  <li>5.1 Algebra</li>
+                </ul>
+              </div>
             </div>
           </div>
         </section>
